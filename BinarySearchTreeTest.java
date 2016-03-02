1c01f7f4f2f49d5757531940d651b8656c6c34dc
gadkins <grayson.w.adkins@gmail.com>
2016-03-02 13:00:36 -0800

    JUnit tests added for add, contains, hasNext and next

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -1,6 +1,7 @@
 package genericBST;
 
 import static org.junit.Assert.*;
+import java.util.TreeSet;
 
 import java.util.*;
 
@@ -9,38 +10,59 @@ import org.junit.Test;
 public class BinarySearchTreeTest {
 
 	@Test
-	public void iterator() {
+	public void BinarySearchTree() {
+		BinarySearchTree<String> tree1 = new BinarySearchTree<String>();
+		BinarySearchTree<String> tree2 = new BinarySearchTree<String>();
+		assertNotNull(tree1);
+		assertNotSame(tree1,tree2);
+	}
+	
+	@Test
+	public void hasNext() {
 		BinarySearchTree<String> tree = new BinarySearchTree<String>();
-//		assertTrue(tree.add("G"));
-//		assertTrue(tree.add("A"));
-//		assertTrue(tree.add("Z"));
-//		assertFalse(tree.add("G"));
-//		assertTrue(tree.contains("A"));
-//		assertFalse(tree.contains("F"));
-		Iterator<String> iterator1 = tree.iterator();
-		assertNotNull(iterator1);
-//		assertFalse(iterator1.hasNext());
-//		String first1 = iterator1.next();
-//		assertNotNull(first1);
-//		Iterator<String> iterator2 = tree.iterator();
-//		String first2 = iterator2.next();
-//		assertSame(first1, first2);
+		assertTrue(tree.add("G"));
+		assertTrue(tree.add("h"));
+		Iterator<String> stringIterator = tree.iterator();
+		assertNotNull(stringIterator);
+		assertTrue(stringIterator.hasNext());
+		assertTrue(stringIterator.hasNext());
+		assertFalse(stringIterator.hasNext());
+	}
+	
+	@Test
+	public void next() {
+		BinarySearchTree<String> tree = new BinarySearchTree<String>();
 	}
 
 	@Test
 	public void add() {
-		BinarySearchTree<String> tree = new BinarySearchTree<String>();
-		assertTrue(tree.add("G"));
-		assertTrue(tree.add("A"));
-		assertTrue(tree.add("Z"));
-		assertFalse(tree.add("G"));
-		assertFalse(tree.add("A"));
+		BinarySearchTree<String> stringTree = new BinarySearchTree<String>();
+		assertTrue(stringTree.add("G"));
+		assertTrue(stringTree.add("a"));
+		assertTrue(stringTree.add("Z"));
+		assertFalse(stringTree.add("G"));
+		assertTrue(stringTree.contains("G"));
+		assertTrue(stringTree.contains("a"));
+		assertTrue(stringTree.contains("Z"));
+		BinarySearchTree<Integer> intTree = new BinarySearchTree<Integer>();
+		assertTrue(intTree.add(5));
+		assertTrue(intTree.add(3));
+		assertTrue(intTree.add(8));
+		assertTrue(intTree.contains(5));
+		assertTrue(intTree.contains(3));
+		assertTrue(intTree.contains(8));
 	}
 	
 	@Test
 	public void contains() {
 		BinarySearchTree<String> tree = new BinarySearchTree<String>();
 		assertTrue(tree.add("G"));
-		assertTrue(tree.contains("G"));
+		assertTrue(tree.add("A"));
+		assertTrue(tree.contains("A"));
+		assertTrue(tree.add("Z"));
+		assertFalse(tree.add("G"));
+		assertTrue(tree.contains("A"));
+		assertFalse(tree.contains("F"));
 	}
+	
 }
